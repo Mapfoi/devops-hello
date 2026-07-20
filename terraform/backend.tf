d179845d9225033ef91d5d2aba9e55a4cd7cde05
Mapfoi <seva.eshmanskii1109@gmail.com>
2026-07-20 17:15:00 +0300

    Configure backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,16 +1,18 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   backend "s3" {
+
     endpoint = "storage.yandexcloud.net"
 
     bucket = "tfstate-mapfoi37"
-    key    = "terraform.tfstate"
+
+    key = "terraform.tfstate"
+
     region = "ru-central1"
 
-    skip_region_validation      = true
+    skip_region_validation = true
+
     skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
+
+    force_path_style = true
   }
 }
